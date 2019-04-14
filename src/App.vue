d63c979f197563f1710242842180e27d461f5b1f
Tanner Damron <tanner.mdamron@yahoo.com>
2019-04-14 15:40:23 -0700

    add small components and v-for to loop through array of objects

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
-<template>
+<template web>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Todos from './components/Todos';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'app',
+    components: {
+      Todos
+    },
+    data() {
+      return {
+        todos: [
+          {
+            id: 1,
+            title: 'Todo One',
+            completed: false
+          },
+          {
+            id: 2,
+            title: 'Todo Two',
+            completed: true
+          },
+          {
+            id: 3,
+            title: 'Todo Three',
+            completed: false
+          }
+        ]
+      };
+    },
+    methods: {
+    }
+  };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
